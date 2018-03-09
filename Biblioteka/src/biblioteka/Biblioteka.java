a8250848b56738a217b90889edebe65c98d5822b
SvetlanaTomic <svetlana.tomic997@gmail.com>
2018-03-09 11:07:20 +0100

    Revert "Dodati kometari"
    This reverts commit 2caa2a241384449557c5ecfc4b88957c3df33276.

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -32,7 +32,6 @@ public class Biblioteka implements BibliotekaInterfejs{
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
-		//komentar novi
 		if(naslov==null )
 			throw new RuntimeException("Morate uneti naslov");
 
